568521d92c2b0568502f4ee561fc19f5c85befd7
Brendan Shumberger <bshumberger@vt.edu>
2023-10-18 15:35:19 -0400

    Updates spectral generation with Lorentzians for both ROA and VCD and included a frequncy factor.

--- SoAPy/functions.py
+++ SoAPy/functions.py
@@ -526,16 +526,20 @@ def generate_spectrum(fwhm, number_of_points, dir_list, dir_parameters, dir_freq
         for j in range(len(spec_intensities)):
             normalized_spec_intensities[j] = spec_intensities[j]*(1/int(dir_parameters[a][5]))
 
-        # Calculating the differential molar extinction coefficient in equation 8d in "ECD Cotton Effects Approximated by the Gaussian Curve and Other Methods" by Philip J. Stephens and Nobuyuki Harada.
+        # Fitting data to line shapes.
+        # See equations 1.16 and 3.51 "Vibrational Optical Activity Principles and Applications" by Laurence Nafie for details.
+        # Note the prefactor in equation 3.51 is excluded.
         if dir_parameters[a][0] == 'VCD':
             for b in range(len(frequency_axis)):
                 for c in range(len(spec_frequencies)):
-                    intensity_axis[b] += (1/((2.296*10**(-39))*np.sqrt(np.pi)*fwhm))*spec_frequencies_eV[c]*normalized_spec_intensities[c]*np.exp(-((frequency_axis_eV[b]-spec_frequencies_eV[c])/fwhm)**2)
-        # Calculating the Lorentzian-fit circular intensity difference from equation 5 and 6 in "Simulation of Raman and Raman optical activity of saccharides in solution" by Palivec et al.
+                    # Equation 8d in "ECD Cotton Effects Approximated by the Gaussian Curve and Other Methods" by Philip J. Stephens and Nobuyuki Harada.
+                    #intensity_axis[b] += (1/((2.296*10**(-39))*np.sqrt(np.pi)*fwhm))*spec_frequencies_eV[c]*normalized_spec_intensities[c]*np.exp(-((frequency_axis_eV[b]-spec_frequencies_eV[c])/fwhm)**2)
+                    intensity_axis[b] += spec_frequencies_eV[c]*normalized_spec_intensities[c]*(fwhm**2/(4*(frequency_axis_eV[b]-spec_frequencies_eV[c])**2+fwhm**2))
         if dir_parameters[a][0] == 'ROA':
             for b in range(len(frequency_axis)):
                 for c in range(len(spec_frequencies)):
-                    intensity_axis[b] += normalized_spec_intensities[c]*(2/np.pi)*(fwhm/(4*(frequency_axis_eV[b]-spec_frequencies_eV[c])**2+fwhm**2))
+                    #intensity_axis[b] += normalized_spec_intensities[c]*(2/np.pi)*(fwhm/(4*(frequency_axis_eV[b]-spec_frequencies_eV[c])**2+fwhm**2))
+                    intensity_axis[b] += spec_frequencies_eV[c]*normalized_spec_intensities[c]*(fwhm**2/(4*(frequency_axis_eV[b]-spec_frequencies_eV[c])**2+fwhm**2))
 
         # NEED TO CHECK UNITS FOR BOTH OF THESE EQUATIONS. THIS INCLUDES UNITS ON FWHM.
         # NEED TO ADD NORMALIZATION FUNCTION FOR THESE EQUATIONS.
