e35d2380d723c3a0c51ffe4aa7c2f8cf526aba80
Ethan Fink <efofficial21@gmail.com>
2024-03-28 13:29:17 -0400

    Beta-tests
    Some initial betas of test functions. The module importing issue needs to be sorted in SoAPy before more development.

--- SoAPy/__init__.py
+++ SoAPy/__init__.py
@@ -1,7 +1,11 @@
 """A package for research on SOlvation Algorithms in PYthon, SoAPy."""
 
 # Add imports here
-from .functions import set_options
+#from ._version import __version__
 
-
-from ._version import __version__
+from SoAPy.functions import *
+from SoAPy.generate_files import *
+from SoAPy.get_geometry import * 
+from SoAPy.analysis import *
+from SoAPy.functions_copy import *
+from SoAPy.collect_data import *
