e38216ab3735c011e0cc78f974eb3094f6b91fbe
Ethan Fink <efofficial21@gmail.com>
2024-03-30 14:40:14 -0400

    Update __init__.py

--- SoAPy/__init__.py
+++ SoAPy/__init__.py
@@ -1,11 +1,7 @@
 """A package for research on SOlvation Algorithms in PYthon, SoAPy."""
 
 # Add imports here
-#from ._version import __version__
 
-from SoAPy.functions import *
-from SoAPy.generate_files import *
-from SoAPy.get_geometry import * 
-from SoAPy.analysis import *
-from SoAPy.functions_copy import *
-from SoAPy.collect_data import *
+
+from ._version import __version__
+
