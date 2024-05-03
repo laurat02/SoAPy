b74871aaf277f23e3cb2cf328a67a6c07b221807
Ethan Fink <efofficial21@gmail.com>
2024-05-03 15:58:16 -0400

    Fixing git diff

--- SoAPy/collect_data.py
+++ SoAPy/collect_data.py
@@ -252,7 +252,6 @@ def generate_VCD_and_ROA_spectra(fwhm, number_of_points, dir_list, dir_parameter
 
 
 
-
 def generate_VCD_and_ROA_convergence_spectra(fwhm, number_of_points, dir_list, dir_parameters, dir_frequencies, dir_intensities, min_frequency, max_frequency, snapshot_list):
     """
     Generates the spectrum for each test assuming only one snapshot test has been performed and only VCD and ROA are being explored (not optical rotation). 
@@ -265,6 +264,9 @@ def generate_VCD_and_ROA_convergence_spectra(fwhm, number_of_points, dir_list, d
     max_intensity = []
     dir_frequencies_new = []
     dir_intensities_new = []
+    dir_snapshot_axis = []
+    dir_integration_axis = []
+
 
     for a in range(len(dir_list)):
         print("Generating spectral data for test", a,".")
@@ -273,6 +275,8 @@ def generate_VCD_and_ROA_convergence_spectra(fwhm, number_of_points, dir_list, d
         dir_frequency_axis.append([])
         dir_intensity_axis.append([])
         max_intensity.append([])
+        dir_snapshot_axis.append([])
+        dir_integration_axis.append([])
 
         for x in range(0,len(snapshot_list)):
             print("Calculating spectrum for",snapshot_list[x],"snapshots.")
@@ -334,7 +338,13 @@ def generate_VCD_and_ROA_convergence_spectra(fwhm, number_of_points, dir_list, d
             dir_frequency_axis[a].append(frequency_axis)
             dir_intensity_axis[a].append(intensity_axis)
 
-    return dir_frequency_axis, dir_intensity_axis, max_intensity
+            dir_snapshot_axis[a].append(snapshot_list[x])
+            dir_integration_axis[a].append(np.trapz(intensity_axis, x = frequency_axis))
+
+            #print(dir_snapshot_axis[a])
+            #print(dir_integration_axis[a])
+
+    return dir_frequency_axis, dir_intensity_axis, max_intensity, dir_snapshot_axis, dir_integration_axis
 
 
 
